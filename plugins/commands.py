087c5cb7e716f457e71ed56b9969a2bc755a507a
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-08-12 23:04:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -32,10 +32,10 @@ async def start(bot, cmd):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("📢 Join Updates Channel 📢", url=invite_link.invite_link)
+                                InlineKeyboardButton("⭕️ Join Updates Channel ⭕️", url=invite_link.invite_link)
                             ],
                             [
-                                InlineKeyboardButton("🔄 Try Again", callback_data=f"checksub#{file_id}")
+                                InlineKeyboardButton("🔄 Try Again⭕️", callback_data=f"checksub#{file_id}")
                             ]
                         ]
                     ),
@@ -67,10 +67,10 @@ async def start(bot, cmd):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('⭕️group⭕️', url='https://t.me/bhddhhddnjd')
                     ],
                     [
-                        InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat='')
+                        InlineKeyboardButton('⭕️🔍 Search again ⭕️', switch_inline_query_current_chat='')
                     ]
                     ]
                 await bot.send_cached_media(
@@ -81,7 +81,7 @@ async def start(bot, cmd):
                     )
         except Exception as err:
             await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
-    elif len(cmd.command) > 1 and cmd.command[1] == 'subscribe':
+    elif len(cmd.command) > 1 and cmd.command[1] == '⭕️subscribe⭕️':
         invite_link = await bot.create_chat_invite_link(int(AUTH_CHANNEL))
         await bot.send_message(
             chat_id=cmd.from_user.id,
@@ -89,7 +89,7 @@ async def start(bot, cmd):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("📢 Join Updates Channel 📢", url=invite_link.invite_link)
+                        InlineKeyboardButton("⭕️ Join Updates Channel ⭕️", url=invite_link.invite_link)
                     ]
                 ]
             )
@@ -102,8 +102,8 @@ async def start(bot, cmd):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Bot Updates", url="https://t.me/Mo_Tech_YT")
+                        InlineKeyboardButton("⭕️Search Here⭕️", switch_inline_query_current_chat=''),
+                        InlineKeyboardButton("⭕️group⭕️", url="https://t.me/bhddhhddnjd")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
@@ -195,8 +195,8 @@ async def delete(bot, message):
 async def bot_info(bot, message):
     buttons = [
         [
-            InlineKeyboardButton('Update Channel', url='https://t.me/Mo_Tech_YT'),
-            InlineKeyboardButton('Video', url=f'{TUTORIAL}')
+            InlineKeyboardButton('⭕️group⭕️', url='https://t.me/bhddhhddnjd'),
+            InlineKeyboardButton('⭕️⭕️⭕️⭕️⭕️⭕️', url=f'https://t.me/bhddhhddnjd')
         ]
         ]
-    await message.reply(text=f"<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='{TUTORIAL}'>Click here</a>\nUpdate Channel : <a href='https://t.me/Mo_Tech_YT'>Mo Tech YT</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text=f"<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://t.me/bhddhhddnjd'>Click here</a>\nUpdate Channel : <a href='https://t.me/bhddhhddnjd'>on air tm</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
