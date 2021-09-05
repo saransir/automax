ca5de22ceef02061720dea916d2f6c318516f76c
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-09-05 12:46:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,6 +36,10 @@ START_MSG = environ.get('START_MSG', default_start_msg)
 
 FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
 OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
+if FILE_CAPTION.strip() == "":
+    CUSTOM_FILE_CAPTION=None
+else:
+    CUSTOM_FILE_CAPTION=FILE_CAPTION
 if OMDB_API_KEY.strip() == "":
     API_KEY=None
 else:
