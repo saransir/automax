28b458499d83365e53649ed4d0039e98ff472808
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-09-25 16:10:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -198,4 +198,4 @@ async def bot_info(bot, message):
             InlineKeyboardButton('⚠️ group rules ⚠️', url='https://t.me/movie_requesting_group_rules/4')
         ]
         ]
-    await message.reply(text=f"Hi)
+    await message.reply(text=f"hi🎡")
