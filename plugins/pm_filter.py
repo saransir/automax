53f35c5d7699c59cfb346c862f1053927ac99f8f
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-10-31 23:52:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -50,12 +50,13 @@ async def filter(client, message):
         return
     if 2 < len(message.text) < 100:    
         btn = []
+        mog = random.choice(IMOG)
         search = message.text
         files = await get_filter_results(query=search)
         if files:
             for file in files:
                 file_id = file.file_id
-                filename = f"🎪-[{get_size(file.file_size)}] {file.file_name}"
+                filename = f"mog-[{get_size(file.file_size)}] {file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")]
                     )
