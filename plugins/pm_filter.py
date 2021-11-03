9eb522a86263fc4151fef5be72d92cb0bfcb3cf1
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-11-03 16:45:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -164,11 +164,11 @@ async def group(client, message):
             if poster:
                 await message.reply_photo(photo=poster, caption=result_txt, reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(10) # in seconds
-                await message.delete(message)
+                await message.delete(bot, message)
             else:
                 await message.reply_text(result_txt, reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(10) # in seconds
-                await message.delete(message)
+                await message.delete(bot, message)
             return
 
         data = BUTTONS[keyword]
