13ff82a12547a9a3336702d98ed8341411a5ea47
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-11-04 17:02:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -169,7 +169,7 @@ async def group(client, message):
             if poster:
                 await message.reply_photo(photo=poster, caption=result_txt, reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(10) # in seconds
-                await message.delete()
+                await query.message.delete()
             else:
                 await message.reply_photo(photo=f"{random.choice(PHOTO)}", caption=result_txt, reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(10) # in seconds
