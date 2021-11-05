394f9a713d21cd2516448ab5aaf5a871aec3eef3
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-11-05 15:53:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -83,8 +83,8 @@ async def filter(client, message):
         if not btn:
             return
 
-        if len(btn) > 10: 
-            btns = list(split_list(btn, 10)) 
+        if len(btn) > 15: 
+            btns = list(split_list(btn, 15)) 
             keyword = f"{message.chat.id}-{message.message_id}"
             BUTTONS[keyword] = {
                 "total" : len(btns),
@@ -153,8 +153,8 @@ async def group(client, message):
         if not btn:
             return
 
-        if len(btn) > 10: 
-            btns = list(split_list(btn, 10)) 
+        if len(btn) > 6: 
+            btns = list(split_list(btn, 6)) 
             keyword = f"{message.chat.id}-{message.message_id}"
             BUTTONS[keyword] = {
                 "total" : len(btns),
