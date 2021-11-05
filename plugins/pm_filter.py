ecc9b4718612def38bdc3a3e68640285ffd5513e
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-11-05 14:13:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -67,13 +67,12 @@ async def filter(client, message):
         return
     if 2 < len(message.text) < 100:    
         btn = []
-        search = message.text
-        onair = "{random.choice(RAT)}"
+        search = message.text 
         files = await get_filter_results(query=search)
         if files:
             for file in files:
                 file_id = file.file_id
-                filename = f"**onair**[{get_size(file.file_size)}] {file.file_name}"
+                filename = f"{random.choice(RAT)}[{get_size(file.file_size)}] {file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")]
                     )
