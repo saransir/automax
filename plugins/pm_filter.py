39e4e6b0b085b5fbdd5322c88f11150cca9724e8
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-11-06 13:15:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -142,7 +142,7 @@ async def group(client, message):
         if files:
             for file in files:
                 file_id = file.file_id
-                filename = f"{random.choice(RAT)}➣[{get_size(file.file_size)}]➪{file.file_name}"
+                filename = f"{random.choice(RAT)}-[{get_size(file.file_size)}]✪{file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}")]
                 )
