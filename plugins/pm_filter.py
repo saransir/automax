251aa38e195da4ff124a053b4d53f0975765d43f
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-11-20 13:01:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -143,7 +143,7 @@ async def group(client, message):
         if files:
             for file in files:
                 file_id = file.file_id
-                filename = f"{oam}-[{get_size(file.file_size)}]✪{file.file_name}"
+                filename = f"{oam}-{get_size(file.file_size)}✪{file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}")]
                 )
