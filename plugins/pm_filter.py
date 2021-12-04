91237e161c117f2937b95fe93a7d5ce890eb2abf
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-12-04 10:48:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -65,7 +65,7 @@ async def filter(client, message):
             return
     if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
         return
-    if 2 < len(message.text) < 100:    
+    if 99 < len(message.text) < 100:    
         btn = []
         search = message.text 
         files = await get_filter_results(query=search)
