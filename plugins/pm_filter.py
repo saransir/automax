6c7a04755b3ddbc2f7c63c165924129e36a84277
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2022-01-08 10:40:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -320,8 +320,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.message.delete()
         elif query.data == "report":
             # await query.edit_message_media(media="https://telegra.ph/file/3916217b15b6760626e02.jpg", caption=f"{query.from_user.mention} id: [ {query.from_user.id}] , \n\n **Reported to Admins** 👮‍♂️")
-            await query.message.edit(text=f"{query.from_user.mention} **#{query.from_user.id}**, \n\n **Reported to Admins** 👮‍♂️", disable_web_page_preview=True)
-            await client.send_message(chat_id="@S1a2r3a4n", text="‼️ {query.from_user.mention} **#{query.from_user.id}**,", disable_web_page_preview=True)
+            await query.message.edit(text=f"{query.from_user.mention} #{query.from_user.id}, \n\n **Reported to Admins** 👮‍♂️", disable_web_page_preview=True)
+            await client.send_message(chat_id="@S1a2r3a4n", text="‼️ {query.message.reply_to_message.from_user.mention} #{query.message.reply_to_message.from_user.id},", disable_web_page_preview=True)
 
 
             # chat_id = query.message.chat.id
