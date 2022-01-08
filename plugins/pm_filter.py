9e403c4391303af7f7102f9bacbed4ce671bdd95
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2022-01-08 12:19:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -206,7 +206,6 @@ async def group(client, message):
             await message.reply_photo(photo=poster, caption=resul_txt, reply_markup=InlineKeyboardMarkup(buttons))
         else:
             await message.reply_photo(photo=f"{random.choice(PHOTO)}", caption=resul_txt, reply_markup=InlineKeyboardMarkup(buttons))
-saran = message.text
     
 def get_size(size):
     """Get size in readable format"""
@@ -320,7 +319,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.message.delete()
         elif query.data == "report":
             # await query.edit_message_media(media="https://telegra.ph/file/3916217b15b6760626e02.jpg", caption=f"{query.from_user.mention} id: [ {query.from_user.id}] , \n\n **Reported to Admins** 👮‍♂️")
-            ac = await query.message.edit(text=f"{query.from_user.mention} [{query.from_user.id}]{saran}, \n **Reported to Admins** 👮‍♂️", disable_web_page_preview=True)
+            ac = await query.message.edit(text=f"{query.from_user.mention} [{query.from_user.id}]{reply_to_message}, \n **Reported to Admins** 👮‍♂️", disable_web_page_preview=True)
             await ac.forward("@S1a2r3a4n")
 
 
