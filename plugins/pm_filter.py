de4cd2e7cef8f8c94456f15b66ae91e0117d190f
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2022-01-22 20:38:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -163,8 +163,8 @@ async def group(client, message):
         if not btn:
             return
 
-        if len(btn) > 12: 
-            btns = list(split_list(btn, 12)) 
+        if len(btn) > 8: 
+            btns = list(split_list(btn, 6)) 
             keyword = f"{message.chat.id}-{message.message_id}"
             BUTTONS[keyword] = {
                 "total" : len(btns),
