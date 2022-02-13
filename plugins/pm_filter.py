c9212044feb7bd23fc1484c81f3c1d3ad426bf62
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2022-02-13 22:29:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -355,7 +355,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             except UserIsBlocked:
                 await query.answer(url=f"http://t.me/On_air_Filter_bot?start=subinps_-_-_-_{file_id}")
             else:
-                await query.answer("The file🎬 has 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈😊 sent to your pm",show_alert=True)
+                await query.answer("ᴄʜᴇcᴋ ᴩᴍ 👀 \n file🎬 has 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈✔️ sent to your pm",show_alert=True)
 
         elif query.data.startswith("checksub"):
             if AUTH_CHANNEL and not await is_subscribed(client, query):
