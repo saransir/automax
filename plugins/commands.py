94ed019ff4d82bf905d979a46d2a195dafb2cb4e
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2022-03-26 14:01:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -121,7 +121,7 @@ async def start(bot, cmd):
         )
     else:
         await cmd.reply_text(
-            START_MSG,
+            START_MSG.format(cmd.from_user.first_name),
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
