8c33f26162e439fff08254a9d3cba93cf04547df
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2022-04-10 23:29:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,4 +1,5 @@
 import os
+import re
 import logging
 import asyncio
 import random
@@ -375,4 +376,11 @@ async def gen_link_s(bot, message):
         return await message.reply("Reply to a supported media")
     file_id, ref = unpack_new_file_id((getattr(replied, file_type)).file_id)
     await message.reply(f"Here is your Link:\nhttps://telegram.dog/On_air_Filter_bot?start=subinps_-_-_-_{file_id}")
-    
+
+@Client.on_message(filters.command('ref') & filters.user(ADMINS))
+async def le_a_chat(bot, message):
+    text = message.command[1]   
+    query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", text, flags=re.IGNORECASE)
+    await message.reply(f"query")
+
+ 
