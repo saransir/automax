d9980f1bbbcdbc85fec09db99a640d7cb88332e4
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2022-04-13 13:39:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -272,7 +272,6 @@ def split_list(l, n):
         yield l[i:i + n]          
 
 async def advantage_spell_chok(message):
-    messagem = message.message.reply_to_message
     query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", message.text, flags=re.IGNORECASE) # plis contribute some common words 
     query = query.strip() + " movie"
     g_s = await search_gagala(query)
@@ -282,7 +281,7 @@ async def advantage_spell_chok(message):
         k = await message.reply("I couldn't find any movie in that name.")
         await asyncio.sleep(8)
         await k.delete()
-        await messagem.delete()
+        await message.delete()
         return
     # regex = re.compile(r".*(imdb|wikipedia).*", re.IGNORECASE) # look for imdb / wiki results
     # gs = list(filter(regex.match, g_s))
