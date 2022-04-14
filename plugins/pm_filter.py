fb2e0e76e1bad45f06da2c2082280d2517656fd6
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2022-04-14 12:49:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -212,9 +212,9 @@ async def group(client, message):
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"saran#{file_id}")]
                 )
         else:
-            return await spell(message)
+            return await advantage_spell_chok(message)
         if not btn:
-            return
+            return await advantage_spell_chok(message)
 
         if len(btn) > 6: 
             btns = list(split_list(btn, 6)) 
