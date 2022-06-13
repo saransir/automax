6e407a2678e0a594a3eb9e435d7c80e29d711ee1
telegramamsecond <104004811+telegramamsecond@users.noreply.github.com>
2022-06-13 12:21:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,6 +33,7 @@ Here you can search files in Inline mode as well as PM, Use the below buttons to
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 IMDB_TEMPLATE = "<b>🎬↳ ɴᴀᴍᴇ: <a href={url}>{title}</a>🤺ɪᴍᴅʙ</b>\n\n <b>‌‌‌‌╔‎/yᴇᴀʀ: {year}\n ╠|ʀᴀᴛɪɴɢ‌‌‌‌‎: {rating}/10‌‌‌‌ \n ╚\ɢᴇɴʀᴇ: #{genres}</b> \n\n     <b>[𝚐𝚛𝚙 1](https://t.me/+PBGW_EV3ldY5YjJl)↮[𝚐𝚛𝚙 2](https://t.me/+eDjzTT2Ua6kwMTI1)</b>"
+IMDB_TEMPLATEE = "ɴᴀᴍᴇ: {title}\n yᴇᴀʀ: {year}\n ʀᴀᴛɪɴɢ‌‌‌‌‎: {rating}/10‌‌‌‌ \n ɢᴇɴʀᴇ: #{genres} \n ʟᴀɴɢᴜᴀɢᴇ: {languages}\n ʀᴇʟᴇᴀsᴇ ᴅᴀᴛᴇ: {release_date}\n ʀᴜɴᴛɪᴍᴇ: {runtime}"
 
 FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
 OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
