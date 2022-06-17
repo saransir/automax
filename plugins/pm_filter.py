ee2e7931528ab0db971a3542ec5747b7be9ec669
telegramamsecond <104004811+telegramamsecond@users.noreply.github.com>
2022-06-17 15:13:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -43,7 +43,7 @@ async def advantage_spoll_choker(bot, query):
         imdb = await get_post(query=movi, id=True)
         tt = imdb.get('title')
         mov = tt.replace(":", "")
-        yea = imdb.get('year') if movie.get else "None"
+        yea = imdb.get('year') if imdb.get else "None"
         movie = f"{mov} {yea}"
         btn.append(
             [InlineKeyboardButton(text="🎪 ɪɴꜰᴏ ",callback_data=f"imdb#tt{movi}")]
