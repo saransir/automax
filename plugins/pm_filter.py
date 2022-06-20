7d21913e5ca292da274302580c67a65ba8f4078c
telegramamsecond <104004811+telegramamsecond@users.noreply.github.com>
2022-06-20 16:08:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -37,7 +37,7 @@ async def advantage_spoll_choker(bot, query):
             await query.answer("You are clicking on an old button which is expired.", show_alert=True)
             return await query.message.delete()
         ttte = movies[(int(movie_))]
-        mov = re.sub(r"(:|-|_|´|,|.|;|IMDb)", "", ttte, flags=re.IGNORECASE)
+        mov = re.sub(r"(:|-|_|;|IMDb)", "", ttte, flags=re.IGNORECASE)
         movie = mov.strip()
         if len(movie) > 30:
             await query.message.edit_text(f"𝑻𝒉𝒊𝒔 𝑴𝒐𝒗𝒊𝒆 𝑵𝒐𝒕 𝑭𝒐𝒖𝒏𝒅 𝑰𝒏 𝑫𝒂𝒕𝒂𝑩𝒂𝒔𝒆💾 \n <spoiler>sᴇᴀʀᴄʜ ɪɴ ɢᴏᴏɢʟᴇ ғᴏʀ ᴄᴏʀʀᴇᴄᴛ sᴘᴇʟʟɪɴɢ</spoiler>")
@@ -49,7 +49,7 @@ async def advantage_spoll_choker(bot, query):
         movi = movie_
         imdb = await get_post(query=movi, id=True)
         ttt = imdb.get('title')[0:29]
-        mov = re.sub(r"(:|-|_|´|,|(|)|.|;|IMDb)", "", ttt, flags=re.IGNORECASE)
+        mov = re.sub(r"(:|-|_|"|;|IMDb)", "", ttt, flags=re.IGNORECASE)
         yea = imdb.get('year')
         movie = f"{mov} {yea}"
         # fils = await get_filter_results(movie)
