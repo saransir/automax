2140c6f6024562586a0fd6d85e43bd21ec4e7140
telegramamsecond <104004811+telegramamsecond@users.noreply.github.com>
2022-08-04 19:22:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -318,7 +318,7 @@ def split_list(l, n):
 async def spell(message):
     titl = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", message.text, flags=re.IGNORECASE) # plis contribute some common words 
     title = titl.strip()
-    fn = titl.replace(" ", "_")
+    fn = titl.replace(" ", "_")[0:30]
     btn = []
     user = message.from_user.id if message.from_user else 0
     movies = await get_post(title, bulk=True)
@@ -340,7 +340,7 @@ async def spell(message):
 
 async def advantage_spell_chok(message):
     query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", message.text, flags=re.IGNORECASE)
-    fn = query.replace(" ", "_")
+    fn = query.replace(" ", "_")[0:30]
     query = query.strip() + " movie"
     g_s = await search_gagala(query)
     g_s += await search_gagala(message.text)
