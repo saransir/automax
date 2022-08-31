3c02db56d91e481337fc3c03da898c05f1aa29cc
telegramamsecond <104004811+telegramamsecond@users.noreply.github.com>
2022-08-31 16:49:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -80,7 +80,7 @@ async def advantage_spoll_choker(bot, query):
         poster=await get_poster(mov)
     if poster:
         try:
-            await query.message.edit_media(poster)
+            await query.message.edit_media(poster, caption=f"🕵‍♂ᴄʜᴇᴄᴋɪɴɢ..")
         except Exception as e:
             print(e)
     if files:
