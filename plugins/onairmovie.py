53a78f46dd3f21240464e4949e0aed82edb1eb4a
Nnvv <87320099+saransir@users.noreply.github.com>
2023-01-04 14:43:52 +0530

    Update onairmovie.py

--- plugins/onairmovie.py
+++ plugins/onairmovie.py
@@ -9,6 +9,22 @@ from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait,
 
 logger = logging.getLogger(__name__)
 
+@Client.on_message(filters.command('invite') & filters.user(ADMINS))
+async def gen_invviite(bot, message):
+    if len(message.command) == 1:
+        return await message.reply('Give me a chat id')
+    chat = message.command[1]
+    try:
+        chat = int(chat)
+    except:
+        return await message.reply('Give Me A Valid Chat ID')
+    try:
+        link = await bot.create_chat_invite_link(chat)
+    except Exception as e:
+        return await message.reply(f'Error {e}')
+    await message.reply(f'**Here is your Invite Link**👇 \n\n {link.invite_link}')
+
+
 @Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
 async def grp_brodcst(bot, message):
     chats = await db.get_all_chats()
