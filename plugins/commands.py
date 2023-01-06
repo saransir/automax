06fbcacffcb76e235b72cf117da72a079b0c47b4
Nnvv <87320099+saransir@users.noreply.github.com>
2023-01-06 14:15:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -387,14 +387,17 @@ async def autoapprove(bot, message: ChatJoinRequest):
     chat=message.chat # Chat
     user=message.from_user # User
     await asyncio.sleep(2)
-    await bot.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    try:
+        await bot.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    except:
+        return
     cg = await bot.send_message(chat_id=chat.id, text=f"ʜɪ {user.mention} \n 💐 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ {chat.title}")
     bantime = int(time.time() + int(10))
     try:
-        await bot.restrict_chat_member(chat_id=chat.id, user_id=user.id, ChatPermissions=ChatPermissions(can_send_messages=True), until_date=bantime)
+        await bot.restrict_chat_member(chat_id=chat.id, user_id=user.id, permissions=ChatPermissions(can_send_messages=True), until_date=bantime)
     except:
         return
-    await asyncio.sleep(16) 
+    await asyncio.sleep(15) 
     await cg.delete()
 @Client.on_message(filters.new_chat_members & filters.group)
 async def auto_welcoime(bot, message):
